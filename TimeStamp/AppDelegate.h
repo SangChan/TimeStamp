ceda2f610fe2ed5e017d94dcc73758a0522d6d0a
SangChan Lee <sangchan82@gmail.com>
2015-04-08 16:12:47 +0900

    initial commit

---
+++ TimeStamp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeStamp
+//
+//  Created by SangChan on 2015. 4. 8..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeStampTests/TimeStampTests.m
@@ -0,0 +1,40 @@
+//
+//  TimeStampTests.m
+//  TimeStampTests
+//
+//  Created by SangChan on 2015. 4. 8..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TimeStampTests : XCTestCase
+
+@end
+
+@implementation TimeStampTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
