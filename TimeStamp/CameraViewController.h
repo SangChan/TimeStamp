37ab3bd6f7ae72547b918903435d0d6a9bb60693
SangChan Lee <sangchan82@gmail.com>
2015-04-10 15:32:07 +0900

    add camera funstion using AVCam example

---
+++ TimeStamp/CameraViewController.h
@@ -0,0 +1,41 @@
+//
+//  CameraViewController.h
+//  TimeStamp
+//
+//  Created by SangChan on 2015. 4. 8..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AVCamPreviewView.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface CameraViewController : UIViewController  <AVCaptureFileOutputRecordingDelegate>
+
+// For use in the storyboards.
+@property (nonatomic, weak) IBOutlet AVCamPreviewView *previewView;
+@property (nonatomic, weak) IBOutlet UIButton *recordButton;
+@property (nonatomic, weak) IBOutlet UIButton *cameraButton;
+@property (nonatomic, weak) IBOutlet UIButton *stillButton;
+
+- (IBAction)toggleMovieRecording:(id)sender;
+- (IBAction)changeCamera:(id)sender;
+- (IBAction)snapStillImage:(id)sender;
+- (IBAction)focusAndExposeTap:(UIGestureRecognizer *)gestureRecognizer;
+
+// Session management.
+@property (nonatomic) dispatch_queue_t sessionQueue; // Communicate with the session and other session objects on this queue.
+@property (nonatomic) AVCaptureSession *session;
+@property (nonatomic) AVCaptureDeviceInput *videoDeviceInput;
+@property (nonatomic) AVCaptureMovieFileOutput *movieFileOutput;
+@property (nonatomic) AVCaptureStillImageOutput *stillImageOutput;
+
+// Utilities.
+@property (nonatomic) UIBackgroundTaskIdentifier backgroundRecordingID;
+@property (nonatomic, getter = isDeviceAuthorized) BOOL deviceAuthorized;
+@property (nonatomic, readonly, getter = isSessionRunningAndDeviceAuthorized) BOOL sessionRunningAndDeviceAuthorized;
+@property (nonatomic) BOOL lockInterfaceRotation;
+@property (nonatomic) id runtimeErrorHandlingObserver;
+
+@end
